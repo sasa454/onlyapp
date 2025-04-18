679d3e6089ea5608a7dcbfd1d4d0ae6060cba208
sasa454 <85271114+sasa454@users.noreply.github.com>
2025-04-18 14:34:11 +0530

    Update main.dart

--- ai_caption_generator (1)/lib/main.dart
+++ ai_caption_generator (1)/lib/main.dart
@@ -1,2 +1,23 @@
 
+name: ai_caption_generator
+description: A new Flutter project.
+publish_to: 'none'
+
+environment:
+  sdk: ">=2.18.0 <3.0.0"
+
+dependencies:
+  flutter:
+    sdk: flutter
+  http: ^0.14.0
+  shared_preferences: ^2.0.15
+  google_mobile_ads: ^2.3.0
+  flutter_clipboard_manager: ^0.0.4
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+
+flutter:
+  uses-material-design: true
 // (main.dart code here, to be filled from the user canvas)
